effed9f9312a275f3f7b3b51af44b8e00b99e034
Zaid Saleem <zaidali36422@gmail.com>
2023-12-13 14:52:24 +0500

    user not found check added in user update route.

--- BE/api/routes/user.js
+++ BE/api/routes/user.js
@@ -9,13 +9,17 @@ route.put('/user', auth, validateUser, async (req, res) => {
     const { user_id } = req.query
     const { username, email, name } = req.body
 
+    const user = await databaseOp.get('SELECT * from user WHERE id = ?', [user_id])
+
+    if (!user) return res.status(404).json({ message: 'User not exist.' })
+
     await databaseOp.update('UPDATE user SET username = ?, email = ?, name = ? WHERE id = ?', [username, email, name, user_id])
 
-    const user = await databaseOp.get('SELECT * FROM user WHERE id = ?', [user_id])
+    const updated_user = await databaseOp.get('SELECT * FROM user WHERE id = ?', [user_id])
 
     delete user.password
 
-    res.status(200).json({ message: 'User updated.', success: true, user })
+    res.status(200).json({ message: 'User updated.', success: true, user: updated_user })
   } catch (error) {
     res.status(500).json({ message: error.message, error })
   }
